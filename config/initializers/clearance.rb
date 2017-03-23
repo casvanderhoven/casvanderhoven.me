9438480a46dcb8115cbddabb004431bf8651c1c2
Cas van der Hoven <casvanderhoven@gmail.com>
2017-03-23 14:52:47 +0100

    Add green colour, change layout slightly, make resume adjustments

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,7 +1,6 @@
 Clearance.configure do |config|
   config.routes = false
   config.mailer_sender = "noreply@casvanderhoven.me"
-  config.rotate_csrf_on_sign_in = true
-  config.cookie_domain = ".heroku.com"
+  config.cookie_domain = ".casvanderhoven.me"
   config.allow_sign_up = false
 end
